b9221ea6aa8ab3875cd69c63480c609b724fe420
Mauricio Perez <delineamientocam@gmail.com>
2024-10-29 22:54:32 -0500

    'initial'

--- index.js
+++ index.js
@@ -1,29 +1,18 @@
 $(function(){
   $("#toggle2").click();
     $("#toggle1").click(function(){
-      $(".modal-mask").fadeIn('slow')
+      $(".modal-mask").fadeIn('slow').
       show();
     });
     
     $("#toggle2").click(function(){
       $(".modal-mask").hide();
     });
+ 
 });
 window.onload = hideModal()
 function hideModal(){
   document.getElementsByClassName("modal-mask").display='none'
 }
-
-function rotateString()
-{
-  var element = document.getElementById('target')
-  var textNode = element.childNodes[0]
-  var text = textNode.data
-  setInterval (
-    function(){
-    text = text[text.length-1] + text.substring(0,text.length-1)
-    textNode.data = text
-  },300)
- 
-} 
-
+const qrContainer = document.getElementById('qr-container')
+new QRCode(qrContainer, 'www.tecnitec.com-bono-15%')
